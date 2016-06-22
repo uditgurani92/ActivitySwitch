834d55a0595a54a7e89023db1e22b01f51b6de3c
uditgurani92 <gurani.udit3@gmail.com>
2016-06-22 13:54:59 +0530

    Activity Switch
    Activity switch using intent

--- app/src/main/java/com/example/udit/activityswitch/FirstActivity.java
+++ app/src/main/java/com/example/udit/activityswitch/FirstActivity.java
@@ -1,13 +1,30 @@
 package com.example.udit.activityswitch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FirstActivity extends AppCompatActivity {
+
+    TextView eName,ePass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        eName=(TextView) findViewById(R.id.eName);
+        ePass=(TextView)findViewById(R.id.ePass);
+
+        Intent i=getIntent();
+        String name=i.getStringExtra("name");
+        String pass=i.getStringExtra("pass");
+        Log.e("FirstActivty",name + "." + pass);
+
+        eName.setText(name);
+        ePass.setText(pass);
     }
 }
--- app/src/main/java/com/example/udit/activityswitch/MainActivity.java
+++ app/src/main/java/com/example/udit/activityswitch/MainActivity.java
@@ -3,42 +3,50 @@ package com.example.udit.activityswitch;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button button1;
-    Button button2;
-    Button button3;
+    Button first_activity;
+    Button second_activity;
+    Button third_activity;
+    EditText enterName,enterPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1=(Button)findViewById(R.id.button1);
-        button2=(Button)findViewById(R.id.button2);
-        button3=(Button)findViewById(R.id.button3);
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
+        first_activity=(Button)findViewById(R.id.button1);
+        second_activity=(Button)findViewById(R.id.button2);
+        third_activity=(Button)findViewById(R.id.button3);
+        enterName=(EditText)findViewById(R.id.edit1);
+        enterPassword=(EditText)findViewById(R.id.edit2);
+        first_activity.setOnClickListener(this);
+        second_activity.setOnClickListener(this);
+        third_activity.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
 
-        if(view==button1)
+        if(view==first_activity)
         {
-            Intent intent=new Intent(this,FirstActivity.class);
+            Intent intent=new Intent(getApplicationContext(),FirstActivity.class);
+            intent.putExtra("name",enterName.getText().toString());
+            intent.putExtra("pass",enterPassword.getText().toString());
+            Log.e("n",enterName.getText()+"."+enterPassword.getText());
             startActivity(intent);
         }
-        if(view==button2)
+        if(view==second_activity)
         {
             Intent intent=new Intent(this,SecondActivity.class);
             startActivity(intent);
         }
-        if(view==button3)
+        if(view==third_activity)
         {
             Intent intent=new Intent(this,ThirdActivity.class);
             startActivity(intent);
